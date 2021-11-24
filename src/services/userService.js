4596272531b6b5c18f571a437be6b229fdf37e33
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-24 18:56:17 -0300

    SignUp route

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,14 +2,13 @@ import bcrypt from 'bcrypt';
 import * as userRepository from '../repositories/userRepository.js';
 
 async function createUser({ name, email, password }) {
-  const hashedPassword = bcrypt.hashSync(password, 12);
-
   const isEmailAlreadyInUse = await userRepository.existingUserWithGivenEmail(
     email
   );
 
   if (isEmailAlreadyInUse) return null;
 
+  const hashedPassword = bcrypt.hashSync(password, 12);
   const user = await userRepository.createUser({ name, email, hashedPassword });
 
   return user;
